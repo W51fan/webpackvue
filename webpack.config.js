28f22eb17186fc06f09b9b6adac9dc9ea2988c33
51fan <abc2978723@163.com>
2018-02-01 16:59:40 +0800

    update webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const isDev = process.env.NODE_ENV === 'development'
+const HTMLplugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const config = {
+	target:"web",
 	entry:__dirname+"/src/index.js",
 	output:{
 		filename:"bundle.js",
@@ -9,7 +14,54 @@ module.exports = {
 			{
 				test:/\.vue$/,
 				use:"vue-loader"
+			},
+			{
+				test:/\.css$/,
+				use:[
+					"style-loader",
+					"css-loader"
+				]
+			},
+			{
+				test:/\.(gif||jpg||jpeg||png||svg)$/,
+				use:[
+					{
+						loader:"url-loader",
+						options:{
+							limit:1024,
+							name:"[name].[ext]"
+						}
+					}
+				]
 			}
 		]
-	}
+	},
+	plugins:[
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: isDev?'"development"':'"production"'
+			}
+		}),
+		new HTMLplugin({
+
+		})
+	]
 }
+
+if(isDev){
+	config.devtool = '#cheap-module-eval-source'
+	config.devServer ={
+		port:8000,
+		host:"0.0.0.0",
+		overlay:{
+			errors:true
+		},
+		hot:true,
+	}
+	config.plugins.push(
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin()
+	)
+}
+
+module.exports = config
