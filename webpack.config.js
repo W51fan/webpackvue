c2e8a64a05b1a4e6e7f7b04a1296fd34584dbc25
51fan <abc2978723@163.com>
2018-02-01 14:22:21 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-	
+	entry:__dirname+"/src/index.js",
+	output:{
+		filename:"bundle.js",
+		path:__dirname+"/dist"
+	},
+	module:{
+		rules:[
+			{
+				test:/\.vue$/,
+				use:"vue-loader"
+			}
+		]
+	}
 }
