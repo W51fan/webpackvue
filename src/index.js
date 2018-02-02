f32b88cfd864a35523938ccfd49034258e5b812f
51fan <abc2978723@163.com>
2018-02-02 16:31:30 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './app.vue'
 
+import './assets/styles/test.css'
+import './assets/images/demo.jpg'
+
 const root = document.createElement('div');
 document.body.appendChild(root);
 
