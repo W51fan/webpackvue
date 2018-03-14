efff35b901c35bb4450efc1cd3e55cad3ca02578
51fan <abc2978723@163.com>
2018-03-14 11:41:41 +0800

    添加vue-router导航

---
+++ src/page04.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="map"></div>
+</template>
+<script>
+    import ol from '../assets/lib/ol.js';
+    import '../assets/styles/ol.css';
+    export default{
+        mounted: function () {
+            let bingMap = new ol.layer.Tile({
+                source: new ol.source.BingMaps({
+                    key: 'AkjzA7OhS4MIBjutL21bkAop7dc41HSE0CNTR5c6HJy8JKc7U9U9RveWJrylD3XJ',
+                    imagerySet: 'Road'
+                })
+            });
+			let map = new ol.Map({
+				layers: [
+					bingMap
+				],
+				target: 'map',
+				view: new ol.View({
+					center: [12959773,4853101],
+					zoom: 12
+				})
+			});
+		},	
+    }
+</script>
