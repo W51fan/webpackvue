a8da571fbd4139a60770182541e5fdfa8a202624
51fan <abc2978723@163.com>
2018-03-14 11:39:47 +0800

    添加vue-router导航

---
+++ src/page01.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="map"></div>
+</template>
+<script>
+    import ol from '../assets/lib/ol.js';
+    import '../assets/styles/ol.css';
+    export default{
+        mounted: function () {
+			let googleMapLayer = new ol.layer.Tile({
+				source:new ol.source.XYZ({
+					url:"http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}"
+				})
+			});
+			let map = new ol.Map({
+				layers: [
+					googleMapLayer
+				],
+				target: 'map',
+				view: new ol.View({
+					center: [12959773,4853101],
+					zoom: 12
+				})
+			});
+		},	
+    }
+</script>
