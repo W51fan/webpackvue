585b35ee6e1b1d68051a789840c422c7b2188d3c
51fan <abc2978723@163.com>
2018-03-14 11:37:58 +0800

    添加vue-router导航

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './app.vue'
+import router from './router/router.js'
 
-import './assets/styles/test.css'
+import './assets/styles/global.styl'
 import './assets/styles/test-stylus.styl'
-import './assets/images/demo.jpg'
+import './assets/images/btbg.png'
+
+
 
 const root = document.createElement('div');
 document.body.appendChild(root);
 
+
 new Vue({
+	router:router,
 	render:(h)=>h(App)
 }).$mount(root);
-
