2ebe33c06edfeef0c4d95069f607c7ea1c96fde4
51fan <abc2978723@163.com>
2018-03-14 11:41:16 +0800

    添加vue-router导航

---
+++ src/page03.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="map"></div>
+</template>
+<script>
+    import ol from '../assets/lib/ol.js';
+    import '../assets/styles/ol.css';
+    export default{
+        mounted: function () {
+            let googleMapLayer = new ol.layer.Tile({
+                source: new ol.source.XYZ({
+                   url:"http://mt0.google.cn/vt/lyrs=m@1&hl=zh-CN&gl=cn&x={x}&y={y}&z={z}&s=Galil"  
+                })
+            })
+			let map = new ol.Map({
+				layers: [
+					googleMapLayer
+				],
+				target: 'map',
+				view: new ol.View({
+					center: [12959773,4853101],
+					zoom: 12
+				})
+			});
+		},	
+    }
+</script>
