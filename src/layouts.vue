f80369dbbf4b7df7fdaa2b9d6bb45be127eb58b4
51fan <abc2978723@163.com>
2018-03-16 16:51:12 +0800

    调整布局

---
+++ src/layouts.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="real-app">
+		<Header></Header>
+		<Left></Left>
+		<Right></Right>
+	</div>
+</template>
+
+<script>
+    import Header from '../common/header.vue'
+    import Left from '../common/left.vue';
+    import Right from '../common/right.vue';
+
+    export default{
+        components:{
+            Header,
+            Left,
+            Right,
+        },
+    }
+</script>
+
+<style lang="stylus" scoped>
+.real-app {
+    margin: 10px auto 
+    box-shadow: 0 0 5px #666
+	}
+</style>
