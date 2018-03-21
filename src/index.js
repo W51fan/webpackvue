3ecc306174daf8c9db815463c382595826a77391
51fan <abc2978723@163.com>
2018-03-21 22:10:32 +0800

    更新文件

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import VueRouter from 'vue-router'
 import App from './app.vue'
 import router from './router/router.js'
 
-//import './assets/styles/global.styl'
-// import './assets/styles/test-stylus.styl'
-// import './assets/images/demo.jpg'
-
-
 
 const root = document.createElement('div');
 document.body.appendChild(root);
