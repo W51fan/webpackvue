fff3a2a763021d0b2dc39da8ffd8252fd23f0ab5
51fan <abc2978723@163.com>
2018-03-30 15:36:47 +0800

    添加vuex

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from './app.vue'
+// import App from './app.vue'
+import App from './components/App.vue'
 import router from './router/router.js'
+import store from './store/index'
 
-//import './assets/styles/global.styl'
-// import './assets/styles/test-stylus.styl'
-// import './assets/images/demo.jpg'
-
-
+Vue.config.productionTip = false
 
 const root = document.createElement('div');
 document.body.appendChild(root);
 
-
 new Vue({
+	store,
 	router:router,
 	render:(h)=>h(App)
 }).$mount(root);
