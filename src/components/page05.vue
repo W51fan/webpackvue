45fa56dc290be410e3cefbb5562fac4ca2ac291f
51fan <abc2978723@163.com>
2018-03-31 16:52:04 +0800

    vue属性绑定

---
+++ src/components/page05.vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+        <span :class="color">target: {{ msg }}</span>
+        <div>
+            <input type="button" value="load" @click="loadingdata()" ></input>
+        </div>
+        <div>
+            <input v-model="inputdata" placeholder="請輸入"></input>
+        </div>
+        <div>
+            <span>{{inputdata}}</span>
+        </div>
+        
+        <Mycheckbox v-model="fruit.peach" value="peach"></Mycheckbox>
+        <Mycheckbox v-model="fruit.apple" value="apple"></Mycheckbox>
+
+        <Singlebox v-model="package" value="peach" name="myFruit"></Singlebox>
+        <Singlebox v-model="package" value="apple" name="myFruit"></Singlebox>
+    </div>
+</template>
+<script>
+import Mycheckbox from "./checkbox.vue";
+import Singlebox from "./singlebox.vue";
+
+
+    export default {
+        name:"Page05",
+        components: {
+            Mycheckbox,
+            Singlebox
+        },
+        data(){
+            return {
+                msg:"hreo",
+                color:"color",
+                inputdata:"",
+                fruit: {//数据
+                    apple: true,
+                    peach: false
+                },
+                package: "peach"
+            }
+        },
+        mounted: function () {},
+        methods:{
+            loadingdata(){
+                alert("ok");
+            },
+        },	
+    }
+</script>
+<style  scoped>
+    .color{
+     background: aquamarine
+    }
+</style>
