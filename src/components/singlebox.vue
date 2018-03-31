43241eae1bc90da7e3f668800d0d3195989c264b
51fan <abc2978723@163.com>
2018-03-31 16:54:31 +0800

    vue属性

---
+++ src/components/singlebox.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <span>{{value}}</span>
+        <input :name="name" type="radio" ref="radio" :checked="checked===value" @change="doThis" :value="value"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Singlebox",
+    data(){
+        
+    },
+    model: {
+        prop: 'checked',
+        event: 'change'
+    },
+    props: {
+        checked: String,
+        value: String,
+        name: String
+    },
+    methods: {
+        doThis() {
+            this.$emit('change', this.$refs.radio.value);
+        }
+    }              
+}
+</script>
+
+<style>
+
+</style>
