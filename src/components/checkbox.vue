5eeeee696bb56b964662df467fb155f0ed9e98b8
51fan <abc2978723@163.com>
2018-03-31 16:54:07 +0800

    vue属性

---
+++ src/components/checkbox.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <span>{{value}}</span>
+        <input type="checkbox" :checked="checked" @change="doThis" :value="value"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Mycheckbox",
+    data(){
+        
+    },
+    model: {
+        prop: 'checked',
+        event: 'change'
+    },
+    props: {
+        checked: Boolean,
+        value: String
+    },
+    methods: {
+        doThis() {
+            this.$emit('change', !this.checked);
+        }
+    }              
+}
+</script>
+
+<style>
+
+</style>
