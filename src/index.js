0d3e0244ed4ccc51b335fe74eb8c66b4ec85573a
51fan <abc2978723@163.com>
2018-04-09 09:52:45 +0800

    添加element-ui

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import App from './components/App.vue'
 import router from './router/router.js'
 import store from './store/index'
 import Axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Axios)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
