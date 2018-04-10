79366564156222ed9a90f3083901406db0039bd3
51fan <abc2978723@163.com>
2018-04-10 15:42:57 +0800

    添加新导航栏

--- src/components/layouts.vue
+++ src/components/layouts.vue
@@ -1,28 +1,61 @@
 <template>
-	<div class="real-app">
-		<Header></Header>
-		<Left></Left>
-		<Right></Right>
+	<div>
+		<el-container>
+            <el-header>Vue demo</el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <navMenu></navMenu>
+                </el-aside>
+                <el-container>
+                <el-main>
+                     <Right></Right>
+                </el-main>
+                <!-- <el-footer>Footer</el-footer> -->
+                </el-container>
+            </el-container>
+        </el-container>
 	</div>
 </template>
 
 <script>
-    import Header from '../common/header.vue'
-    import Left from '../common/left.vue';
-    import Right from '../common/right.vue';
-
+import Header from '../common/header.vue';
+import navMenu from '../common/navMenu.vue';
+import Right from '../common/right.vue';
+import {Button, Container, Header, Main, Aside, Input} from 'element-ui'
     export default{
         components:{
             Header,
-            Left,
-            Right,
+            navMenu,
+            Right
         },
     }
 </script>
 
 <style lang="stylus" scoped>
-.real-app {
-    margin: 10px auto 
-    box-shadow: 0 0 5px #666
-	}
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  .el-input__inner{
+      width: 200px;
+  }
+  body > .el-container {
+    margin-bottom: 40px;
+  }
 </style>
