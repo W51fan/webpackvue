d9fd9208982af628c26294cec7b7a213df04d12d
51fan <abc2978723@163.com>
2018-04-10 17:29:07 +0800

    添加菜单配置

---
+++ src/menu-config.js
@@ -0,0 +1,46 @@
+module.exports = [
+    {
+        name: '地图',
+        id: 'basic',
+        sub: [
+                {
+                    name: '高德',
+                    componentName: '/detail/1-1'
+                }, 
+                {
+                    name: '百度',
+                    componentName: '/detail/1-2'
+                },
+                {
+                    name: '谷歌',
+                    componentName: '/detail/1-3'
+                },
+                {
+                    name: '必应',
+                    componentName: '/detail/1-4'
+                }
+            ]
+        }, 
+    {
+        name: 'Echarts',
+        id: 'form',
+        sub: [
+                {
+                    name: '雷达图',
+                    componentName: '/detail/2-1'
+                }, 
+                {
+                    name: '柱形图',
+                    componentName: '/detail/2-2'
+                },
+                {
+                    name: '饼形图',
+                    componentName: '/detail/2-3'
+                },
+                {
+                    name: '散点图',
+                    componentName: '/detail/2-4'
+                }
+        ]
+    }
+]
