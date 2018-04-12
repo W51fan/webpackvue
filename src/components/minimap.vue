f1cd320d037659eb608bcf9e0f16204c1593a585
51fan <abc2978723@163.com>
2018-04-12 16:44:45 +0800

    新增内容

---
+++ src/components/minimap.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="miniContent">
+        <page09></page09>
+    </div>
+</template>
+
+<script>
+import page09 from './page09.vue'
+export default {
+  name: "minimap",
+  components:{
+      page09
+  },
+};
+</script>
+
+<style scoped>
+.miniContent {
+  background-color: #82afd2a8;
+  width: 500px;
+  height: 500px;
+  position: absolute;
+  z-index: 999999;
+  top: 90px;
+  right: 45px;
+}
+</style>
+
+
+
